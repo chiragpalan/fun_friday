29dadddfee47fb3878fb28cb34838b6a6185330b
chirag <61842354+chiragpalan@users.noreply.github.com>
2025-02-23 01:00:23 +0530

    Update guess_the_image_v5.py

--- guess_the_image_v5.py
+++ guess_the_image_v5.py
@@ -160,7 +160,7 @@ USED_FOLDER = "used_images"
 os.makedirs(USED_FOLDER, exist_ok=True)
 
 # JSON file with personality summaries
-SUMMARY_JSON = "personality_summaries_v2.json"
+SUMMARY_JSON = "personality_summaries_v1.json"
 if os.path.exists(SUMMARY_JSON):
     with open(SUMMARY_JSON, "r", encoding="utf-8") as f:
         summaries = json.load(f)
